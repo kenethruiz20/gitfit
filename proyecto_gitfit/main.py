94ca332bec1d28cf8b6378bc9d13de61914bf460
Milton Beltrán <miltonbeltran@ufm.edu>
2022-04-22 21:54:31 +0200

    Cambios al main.py y al readme
    Los cambios fueron en la forma en que imprime el directorio, no puedo hacer que muestre los archivos dentro de la subcarpeta (aun). Opcion dos creo el readme y lo va cambiando

--- proyecto_gitfit/main.py
+++ proyecto_gitfit/main.py
@@ -8,8 +8,23 @@ import os
 #     data = 'El kenny es un cule hueco'
 #     print(data)
 
-print('\nDirectorio de archivos de la carpeta:\n')
 
-with os.scandir('/Users/milton/Desktop/proyecto_gitfit/gitfit/proyecto_gitfit') as entries:
-    for entry in entries:
-        print(entry.name)
+print('\nBienvenido a Git Fit =)\n')
+print('Elija la operacion que desea realizar:')
+print('1) DIRECTORIO DE ARCHIVOS')
+print('2) AGREGAR ARCHIVOS DE TEXTO')
+menu_option = int(input('\nIngrese: '))
+if menu_option == 1:
+    print('\nDirectorio de archivos de la carpeta:\n') 
+    basepath = ''
+    with os.scandir('/Users/milton/Desktop/proyecto_gitfit/gitfit') as entries:
+        for entry in entries:
+            print(entry.name)
+
+if menu_option == 2:
+    with open('readme.txt', 'w') as f:
+        user_text = str(input(''))
+        f.write(user_text)
+        print(user_text)
+
+    
